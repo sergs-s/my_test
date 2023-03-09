ff7230ea3b2b559752071fca8448b600f3db4ed0
sergs-s <craft2001@mail.ru>
2023-03-09 21:45:12 +0300

    1 commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author ssv
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[][] mass = {
+            {11, 12, 13},
+            {21, 22, 23},
+            {31, 32, 33}
+        };
+        
+//        System.out.println("aaa" + "!");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+               System.out.print(String.valueOf(mass[i][j+1])+" ");
+            }
+            System.out.println("");
+            
+        }
+    }
+    
+}
