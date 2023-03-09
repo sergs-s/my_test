5f474eddcaf584f222bdd0ec41ec5b31ad85c407
sergs-s <craft2001@mail.ru>
2023-03-09 21:48:29 +0300

    1 commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -24,7 +24,7 @@ public class JavaApplication1 {
 //        System.out.println("aaa" + "!");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-               System.out.print(String.valueOf(mass[i][j+1])+" ");
+               System.out.print(String.valueOf(mass[i][j])+" ");
             }
             System.out.println("");
             
